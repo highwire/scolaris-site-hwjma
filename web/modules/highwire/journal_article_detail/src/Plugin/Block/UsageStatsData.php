508d8e15bd8fc92493a184f6b100a78e9f44c533
Rajat Gupta <rajat.gupta@highwirepress.com>
2023-08-03 04:57:56 -0400

    JCOREX-102 : info metric tab

--- web/modules/highwire/journal_article_detail/src/Plugin/Block/UsageStatsData.php
+++ web/modules/highwire/journal_article_detail/src/Plugin/Block/UsageStatsData.php
@@ -11,7 +11,7 @@ use Drupal\Core\Entity\EntityStorageInterface;
 use HighWire\Clients\UsageStats\UsageStats;
 
 /**
- * Provides a 'UsageStatsData' block.
+ * Provides a 'UsageStatsData' block: JCOREX-102
  *
  * @Block(
  *  id = "hwjma_usage_stats",
@@ -58,7 +58,8 @@ class UsageStatsData extends BlockBase implements ContainerFactoryPluginInterfac
     'pdf' => 'PDF',
     'powerpoint' => 'PowerPoint',
     'total' => 'Total',
-    'grand_total' => 'Grand Total'
+    'grand_total' => 'Grand Total',
+    'source' => 'Source'
   ];
 
   /**
@@ -110,7 +111,7 @@ class UsageStatsData extends BlockBase implements ContainerFactoryPluginInterfac
    */
   public static function defaultSettings() {
     return [
-      'query_type' => 'monthly',
+      'query_type' => 'ArticleLifetime',
       'source' => 'highwire-pmc',
       'source_total' => 0,
       'views' => ['abstract', 'full', 'pdf', 'total'],
@@ -120,99 +121,24 @@ class UsageStatsData extends BlockBase implements ContainerFactoryPluginInterfac
   }
 
   /**
-   * @inheritdoc
-   */
-  public function blockForm($form, FormStateInterface $form_state) {
-    $form = parent::blockForm($form, $form_state);
-    $config = $this->getConfiguration();
-
-    $form['query_type'] = [
-      '#type' => 'select',
-      '#required' => TRUE,
-      '#multiple' => FALSE,
-      '#title' => $this->t('Query Type'),
-      '#description' => $this->t('Display statistics monthly or daily?'),
-      '#options' => [
-        'monthly' => 'Monthly Summary',
-        'daily' => 'Daily (limit one year)',
-      ],
-      '#default_value' => isset($config['query_type']) ? $config['query_type'] : 'monthly',
-    ];
-
-    $form['views'] = [
-      '#type' => 'checkboxes',
-      '#required' => TRUE,
-      '#multiple' => TRUE,
-      '#title' => $this->t('Views'),
-      '#description' => $this->t('Display statistics for these views'),
-      '#options' => [
-        'abstract' => 'Abstract',
-        'full' => 'Full Text',
-        'pdf' => 'PDF',
-        'powerpoint' => 'Powerpoint',
-        'total' => 'Total',
-      ],
-      '#default_value' => isset($config['views']) ? $config['views'] : ['abstract', 'full', 'pdf', 'total'],
-    ];
-
-    $date_formats = [];
-    foreach ($this->dateFormatStorage->loadMultiple() as $machine_name => $value) {
-      $date_formats[$machine_name] = $this->t('@name format: @date', ['@name' => $value->label(), '@date' => $this->dateFormatter->format(REQUEST_TIME, $machine_name)]);
-    }
-    $date_formats['custom'] = $this->t('Custom');
-
-    $form['date_format'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Date format'),
-      '#options' => $date_formats,
-      '#default_value' => isset($config['date_format']) ? $config['date_format'] : 'medium',
-    ];
-
-    $form['custom_date_format'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Custom date format'),
-      '#description' => $this->t('See <a href="http://php.net/manual/function.date.php" target="_blank">the documentation for PHP date formats</a>.'),
-      '#default_value' => isset($config['custom_date_format']) ? $config['custom_date_format'] : '',
-    ];
-    $form['custom_date_format']['#states'] = [
-      'visible' => [
-          'select[name="settings[date_format]"]' => ['value' => 'custom']
-      ]
-    ];
-
-    $form['limit'] = [
-      '#type' => 'numeric',
-      '#title' => $this->t('Limit'),
-      '#description' => $this->t('Limit to this number of rows'),
-      '#default_value' => isset($config['limit']) ? $config['limit'] : '',
-    ];
-
-    return $form;
-  }
-
-  /**
-   * @inheritdoc
-   */
-  public function blockSubmit($form, FormStateInterface $form_state) {
-    parent::blockSubmit($form, $form_state);
-    $values = $form_state->getValues();
-    foreach (['query_type', 'source', 'source_total', 'views', 'date_format', 'custom_date_format'] as $option) {
-      if (is_array($values[$option])) {
-        $values[$option] = array_filter($values[$option]);
-      }
-      $this->configuration[$option] = $values[$option];
-    }
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function build() {
     $build = [];
-    
     $node = $this->getContextValue('node');
     $config = $this->getConfiguration();
-    
+    $usageStatsConfig = \Drupal::config('journal_article_detail.settings');
+    $emptyErrorText = $usageStatsConfig->get('empty_text');
+    $showSource = $usageStatsConfig->get('show_source');
+    $defaultSourceName = $usageStatsConfig->get('default_source_name');
+    $usageMetricTypesFilter = $usageStatsConfig->get('metric_types');
+    $displayGrandTotal = $usageStatsConfig->get('display_grand_total');
+    $errorDisplay = [
+      '#type' => 'markup',
+      '#markup' => $emptyErrorText,
+      '#prefix' => '<div class="ajax-wrapper error-msg">',
+      '#suffix' => '</div>',
+    ];
     // Date format
     if ($config['date_format'] == 'custom') {
       $date_format = $config['custom_date_format'];
@@ -221,66 +147,85 @@ class UsageStatsData extends BlockBase implements ContainerFactoryPluginInterfac
       $format_entity = $this->dateFormatStorage->load($config['date_format']);
       $date_format = $format_entity->getPattern();
     }
-
     // Apath
-    //$apath = $node && $node->hasField('apath') && !$node->get('apath')->isEmpty() ? $node->get('apath')->first()->getString() : '';
-    $apath = '/biorxiv/early/2017/03/15/117101.atom';
+    $apath = $node && $node->hasField('apath') && !$node->get('apath')->isEmpty() ? $node->get('apath')->first()->getString() : '';
     if (empty($apath)) {
-      return $build;
+      return $errorDisplay;
     }
-    
     // Get usage Stats
     $stats = [];
+    $options = [];
+    // Check date fromat 
+    if (!empty($config['fromDate']) && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$config['fromDate'])) {
+      $options['from-date'] = $config['fromDate'];
+    }
+    if (!empty($config['toDate']) && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$config['toDate'])) {
+      $options['to-date'] = $config['toDate'];
+    }
     try {
-      if ($config['query_type'] == 'monthly') {
-        $stats = $this->usageStats->singleArticle($apath)->getData();
-      }
-      if ($config['query_type'] == 'daily') {
-        $stats = $this->usageStats->singleArticleYear($apath)->getData();
-      }
+        $stats = $this->usageStats->singleArticle($apath, 'true', $options)->getData();
     }
     catch (\Exception $e) {
       return $build;
     }
     if (empty($stats)) {
-      return $build;
+      return $errorDisplay;
     }
-
     // Render table
     $build['usage_stats_data'] = [
       '#type' => 'table',
-      '#caption' => $this->t('Article Usage Data'),
-      '#prefix' => '<div class="ajax-wrapper">',
+      '#prefix' => '<div class="ajax-wrapper usage-stats-data-table">',
       '#suffix' => '</div>',
     ];
     $build['usage_stats_data']['#header'] = [
       'date' => 'Period',
     ];
+    $counter = 0;
     foreach ($config['views'] as $view) {
       $build['usage_stats_data']['#header'][$view] = $this->view_labels[$view];
     }
-    //dump($stats);
+    $grand_total = [];
     foreach ($stats as $i => $row) {
       $build['usage_stats_data'][$i] = [];
-
       $date = $this->renderDate($date_format, $row['year'], $row['month'], $row['day'] ?? NULL);
       $build['usage_stats_data'][$i]['date']['#markup'] = $date;
-
       $total = 0;
       foreach ($config['views'] as $view) {
         if (isset($row[$view])) {
           $build['usage_stats_data'][$i][$view]['#markup'] = $row[$view];
+          $grand_total[$view] += $row[$view];
           $total += $row[$view];
         }
       }
       if (in_array('total', $config['views'])) {
         $build['usage_stats_data'][$i]['total']['#markup'] = $total;
+        $aaaa += $total;
+      }
+      // Add source if required.
+      if (!empty($usageMetricTypesFilter['source']) && in_array('source', $config['views'])) {
+        // Override default source if set.
+        if ($row['platform'] == 'highwire' && !empty($defaultSourceName)) {
+          $build['usage_stats_data'][$i]['source']['#markup'] = $defaultSourceName;
+        }
+        else {
+          $build['usage_stats_data'][$i]['source']['#markup'] = $row['platform'];
+        }
+      }
+      $counter = $i;
+    }
+    // Add grand total row if required
+    if ($displayGrandTotal == 1) {
+      $build['usage_stats_data'][$counter+1]['date']['#markup'] = 'Grand Total';
+      $innerCounter = 0;
+      foreach ($grand_total as $k => $aa) {
+        $build['usage_stats_data'][$counter+1][$k]['#markup'] = $aa;
+        $innerCounter = $k;
+      }
+      if (in_array('total', $config['views'])) {
+        $build['usage_stats_data'][$counter+1][$innerCounter+1]['#markup'] = $aaaa;
       }
     }
-
     $build['#attached']['library'][] = 'journal_article_detail/usagestatsdata';    
-  //  $build['#theme'] = 'hwjma_usage_stats_data';
-    //dump($build);
     return $build;
   }
 
@@ -319,5 +264,4 @@ class UsageStatsData extends BlockBase implements ContainerFactoryPluginInterfac
     $date->setDate($year, $month, $day ?? 1);
     return $date->format($date_format);
   }
-
 }
