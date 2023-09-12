9e478a0fdfe6cb8d9ba317fcc5768b0f291fd2a3
Rajat Gupta <rajat.gupta@highwirepress.com>
2023-09-12 02:44:29 -0400

    JCOREX-342: Article Print

--- web/modules/highwire/journal_article_detail/js/articleprint.js
+++ web/modules/highwire/journal_article_detail/js/articleprint.js
@@ -10,14 +10,9 @@
         }).join("");
         var article_doi = $('.journal-doi-info').html();
         var article_content = $('.article__tabcontent-body').html();
-        var footer_data = document.getElementsByClassName("footer-menu");
-        var footer = [].map.call( footer_data, function(node){
-            return node.innerHTML;
-        }).join("");
-        var article_footer_bottom = $('.footer__bottom').html();
         // Print article data from variables
         var print_area = window.open();
-        print_area.document.write( '<div><p><h5>'+ article_subtitle +'</h5></p></div><div><p><h3>' + article_title +'</h3></p></div><div>' + article_author + '</div><div>' + article_doi + '</div><div><p>' + article_content + '</p></div><div><p>' + footer + '</p></div><div><p>'+ article_footer_bottom + '</p></div>');
+        print_area.document.write( '<div><p><h5>'+ article_subtitle +'</h5></p></div><div><p><h3>' + article_title +'</h3></p></div><div>' + article_author + '</div><div>' + article_doi + '</div><div><p>' + article_content + '</p></div>');
         print_area.document.close();
         print_area.focus();
         print_area.print();
