5ff92c1a15104dbca79c17fa8c88e30dd6574537
charanjeet <charanjeet.singh@highwirepress.com>
2023-09-27 05:16:38 -0400

    Add comments

--- web/modules/highwire/journal_article_detail/js/pastissue.js
+++ web/modules/highwire/journal_article_detail/js/pastissue.js
@@ -1,34 +1,56 @@
 (function ($) {
-
+    // Event handler for clicking on volume items
     $(document).on('click', ".view-past-issue .vol-list .volumes-browser-slider li", function() {
+        // Get the selected text
         var selectval = $(this).text();
+        
+        // Set the input value to the selected text
         $(".view-past-issue .form-item-volume-int input").val(selectval);
         $(".view-past-issue .form-item-date-ppub-year input").val('');
+        
+        // Remove the 'active' class from all volume items and add it to the clicked item
         $('.volumes-browser-slider li').removeClass('active');
         $(this).addClass('active');
-        $('.view-past-issue .form-actions .btn.btn-search').click();
-    });
-    
-    $(document).on('click', ".view-past-issue .year-list .year-sub-tabs li", function() {
-        var selectval = $(this).text();
-        $(".view-past-issue .form-item-date-ppub-year input").val(selectval);
-        $(".view-past-issue .form-item-volume-int input").val('');
-        $('.year-sub-tabs li').removeClass('active');
-        $(this).addClass('active');
+        
+        // Trigger a click on the search button
         $('.view-past-issue .form-actions .btn.btn-search').click();
     });
 
+    // Event handler for clicking on year items in year
+    $(document).on('click', ".view-past-issue .year-list .year-sub-tabs li", function() {
+        var selectval = $(this).text();
+        
+        // Set the input value to the selected text
+        $(".view-past-issue .form-item-date-ppub-year input").val(selectval);
+        $(".view-past-issue .form-item-volume-int input").val('');
+        
+        // Remove the 'active' class from all year sub items and add it to the clicked item
+        $('.year-sub-tabs li').removeClass('active');
+        $(this).addClass('active');
+        
+        // Trigger a click on the search button
+        $('.view-past-issue .form-actions .btn.btn-search').click();
+    });
+
+    // Event handler for clicking on year items in year tabs
     $(document).on('click', ".view-past-issue .year-list .year-nav-tabs li", function() {
         var selectval = $(this).text();
+        
+        // Hide all year-child elements and show the one with the corresponding ID
         $('.year-child').hide();
         $('.year-sub-slider #'+selectval).show();
+        
+        // Remove the 'active' class from all year nav-tab items and add it to the clicked item
         $('.year-nav-tabs li').removeClass('active');
         $(this).addClass('active');
     }); 
     
+    // Event handler for changing the value of the browse_by select element
     $(document).on('change', ".browse_by select", function() {
         var selectval = $(this).val();
-        if(selectval == 'year'){
+        if (selectval == 'year') {
+            // If year is selected, hide volume-related elements and set the input value
+            // to the last year sub-tab item, then trigger a search button click
             $('.year-child').hide();
             $('.year-list').show();
             $('.vol-list').hide();
@@ -38,8 +60,9 @@
             $('.year-sub-tabs li').removeClass('active');
             $('.year-sub-slider li').last().addClass('active');
             $('.view-past-issue .form-actions .btn.btn-search').click();
-        }
-        else if(selectval == 'volume'){
+        } else if (selectval == 'volume') {
+            // If 'volume' is selected, hide year-related elements and set the input value
+            // to the last volume item, then trigger a search button click
             $('.year-list').hide();
             $('.vol-list').show();
             var val = $('.volumes-browser-slider li').last().text();
@@ -51,23 +74,23 @@
         }
     });
 
-    if($(".view-past-issue .form-item-volume-int input").val() != ''){
+    // Check the initial state of the show_cover input and toggle the corresponding elements
+    if ($(".view-past-issue .form-item-volume-int input").val() != '') {
         var id = $(".view-past-issue .form-item-volume-int input").val();
         $('.year-list').hide();
         $('.vol-list').show();
         $(".browse_by select").val('volume');
         $('.volumes-browser-slider li').removeClass('active');
         $('.volumes-browser-slider li#'+id).addClass('active');
-        $('.past-ssue-heading').text('Volume '+id);
-    }
-    else {
+        $('.past-ssue-heading').text('Volume ' + id);
+    } else {
         var id = $(".view-past-issue .form-item-date-ppub-year input").val();
         $('.year-child').hide();
         $('.year-list').show();
         $('.vol-list').hide();
         $(".browse_by select").val('year');
-        $('.year-list li').removeClass('active'); 
-        if(id != ''){
+        $('.year-list li').removeClass('active');
+        if (id != '') {
             var parent = $('.year-list .year-sub-tabs li#'+id).parents('.year-child').attr('id');  
             $('.year-list .year-sub-tabs li#'+id).addClass('active'); 
             $('.year-list .year-browser-slider li#'+parent+'_year').addClass('active');
@@ -76,17 +99,21 @@
         $('.past-issue-heading').text(id);
     }
 
+    // Event handler for changing the state of the 'show_cover' input
     $(document).on('change', ".show_cover input.switch-input", function() {
-        if($(this).is(':checked')) { 
+        if ($(this).is(':checked')) { 
+            // Show cover image and add a class to the archive issue detail element
             $('.show-cover-image').show();
             $('.archive-issue-detail').addClass('show-issue-img');
         } else {
+            // Hide cover image and remove the class from the archive issue detail element
             $('.show-cover-image').hide();
             $('.archive-issue-detail').removeClass('show-issue-img');
         }
     });
 
-    if($(".show_cover input.switch-input").is(':checked')) { 
+    // Check the initial state of the 'show_cover' input and toggle the corresponding elements
+    if ($(".show_cover input.switch-input").is(':checked')) { 
         $('.show-cover-image').show();
         $('.archive-issue-detail').addClass('show-issue-img');
     } else {
@@ -94,34 +121,35 @@
         $('.archive-issue-detail').removeClass('show-issue-img');
     }
 
+    // Event handler for handling AJAX complete event
     $(document).ajaxComplete(function(){
-		if($(".view-past-issue .form-item-volume-int input").val() != ''){
-			var id = $(".view-past-issue .form-item-volume-int input").val();
-			$('.year-list').hide();
-			$('.vol-list').show();
-			$(".browse_by select").val('volume');
-			$('.volumes-browser-slider li').removeClass('active');
-			$('.volumes-browser-slider li#'+id).addClass('active');
-			$('.past-issue-heading').text('Volume '+id);
-		}
-		else {
-			var id = $(".view-past-issue .form-item-date-ppub-year input").val();
+        if ($(".view-past-issue .form-item-volume-int input").val() != '') {
+            var id = $(".view-past-issue .form-item-volume-int input").val();
+            $('.year-list').hide();
+            $('.vol-list').show();
+            $(".browse_by select").val('volume');
+            $('.volumes-browser-slider li').removeClass('active');
+            $('.volumes-browser-slider li#'+id).addClass('active');
+            $('.past-issue-heading').text('Volume ' + id);
+        } else {
+            var id = $(".view-past-issue .form-item-date-ppub-year input").val();
             $('.year-child').hide();
-			$('.year-list').show();
-			$('.vol-list').hide();
-			$(".browse_by select").val('year');
-			$('.year-list li').removeClass('active');
-            if(id != ''){
+            $('.year-list').show();
+            $('.vol-list').hide();
+            $(".browse_by select").val('year');
+            $('.year-list li').removeClass('active');
+            if (id != '') {
                 var parent = $('.year-list .year-sub-tabs li#'+id).parents('.year-child').attr('id');  
-                $('.year-list .year-sub-tabs li#'+id).addClass('active');  console.log(parent);
+                $('.year-list .year-sub-tabs li#'+id).addClass('active');  
                 $('.year-list .year-browser-slider li#'+parent+'_year').addClass('active');
                 $('.year-sub-slider #'+parent).show();
             }
-		$('.past-issue-heading').text(id);
-		}
-	});
+            $('.past-issue-heading').text(id);
+        }
+    });
 
-    if($(".show_cover input.switch-input").is(':checked')) { 
+    // Check the initial state of the 'show_cover' input and toggle the corresponding elements
+    if ($(".show_cover input.switch-input").is(':checked')) { 
         $('.show-cover-image').show();
         $('.archive-issue-detail').addClass('show-issue-img');
     } else {
