cfd556428018f85236de13bc2f1d3d99004bc55e
Rajat Gupta <rajat.gupta@highwirepress.com>
2023-10-16 06:15:00 -0400

    JCOREX-352:eletters

--- web/modules/highwire/journal_article_detail/js/article_response_tab.js
+++ web/modules/highwire/journal_article_detail/js/article_response_tab.js
@@ -3,8 +3,17 @@
     $(document).on("click", "#eletter-submit-form", function(){
         $('.node-eletters-form').toggle();
     });
+
     $(document).on("click", ".show-click-toggle", function(){
         $(this).parent().siblings('.eletter-field-body').toggle();
+    });
+
+    $(document).on("click", "input[name='field_no_competing_interests']", function(){
+        if ( $(this).val() == 0) {
+            $('.field--name-field-please-describe-the-compet').hide();
+        } else {
+            $('.field--name-field-please-describe-the-compet').show();
+        }
     });
 }(jQuery));
 /* ---------------- Article Response tab : JCOREX-352 ---------------- */
